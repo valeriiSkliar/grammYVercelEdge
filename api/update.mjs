3dbe60f4ff9fb3bafe9a5a76ebe0e7c50838f7f6
vs <valeriisklyarov@gmail.com>
2024-09-08 16:54:16 +0300

    update vercel settings

--- api/update.mjs
+++ api/update.mjs
@@ -1,5 +1,5 @@
-import {bot, secretToken} from "../src/bot.mjs";
-import {webhookCallback} from "grammy";
+import { webhookCallback } from "grammy";
+import { bot, secretToken } from "../src/bot.mjs";
 
 export const config = {runtime: "edge"};
 
@@ -8,3 +8,10 @@ export const POST = webhookCallback(bot, "std/http", {
     timeoutMilliseconds: 24_000,
     secretToken,
 });
+
+export default async function (request) {
+    if (request.method === "POST") {
+        return handler(request);
+    }
+    return new Response("Expected a POST request.", { status: 405 });
+}
